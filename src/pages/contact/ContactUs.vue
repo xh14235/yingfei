1cb3466a198ccbffe3abc7227bc32afd80c0478d
解辉 <15021161540@163.com>
2022-07-18 09:00:53 +0800

    555555555555

--- src/pages/contact/ContactUs.vue
+++ src/pages/contact/ContactUs.vue
@@ -52,7 +52,7 @@
           ></el-alert>
           <el-form-item>
             <el-button type="primary" @click="onSubmit">立即创建</el-button>
-            <el-button>取消</el-button>
+            <el-button>取消11111</el-button>
           </el-form-item>
         </el-form>
       </el-row>
